407ab23aa33b419befa90c8298aab72354d66ce0
lzj112 <872575628@qq.com>
2019-05-13 09:08:49 +0800

    hello world

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年05月13日 星期一 09时07分46秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+    cout << "hello world" << endl;
+   
+    return 0;
+}
